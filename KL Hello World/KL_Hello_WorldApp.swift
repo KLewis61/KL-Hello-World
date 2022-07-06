5c99ec40f3e61ddc9bfdea84ce1cfb3a554f0e31
Kevin Lewis <klewis@nowsecure.com>
2022-07-06 11:48:20 -0500

    Initial Commit

---
+++ KL Hello World/KL_Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  KL_Hello_WorldApp.swift
+//  KL Hello World
+//
+//  Created by Kevin Lewis on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct KL_Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
